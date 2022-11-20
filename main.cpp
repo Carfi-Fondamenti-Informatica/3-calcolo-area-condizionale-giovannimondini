7f643d9fe7e9d186db31f630dcddbef5c6525e26
giovannimondini <117928503+giovannimondini@users.noreply.github.com>
2022-11-20 21:06:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 
 int main() {
-float a=0;
-float b=0;
-int opzione=0;
-cin>>a>>b>>opzione;
-   switch(opzione){
-      case 0:
-         cout<<((a*b)/2);
-         break;
-      case 1:
-         cout<< (a*a);
-         break;
-      case 2:
-         cout<<(a*b);
-      default: cout<<"opzione non valida"<< endl;
+ float a = 0;
+    float b = 0;
+    int opzione = 0;
+    cin >> a >> b >> opzione;
+    switch (opzione) {
+        case 0:
+            cout << ((a * b) / 2);
+            break;
+        case 1:
+            cout << (a * a);
+            break;
+        case 2:
+            cout << (a * b);
+        default:
+            cout << "opzione non valida" << endl;
          
          
    
